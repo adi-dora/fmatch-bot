a4d3517aa0b88cca45ca8277fa5487ee9debb767
Allton274 <adityadora277@gmail.com>
2023-12-18 22:27:08 -0500

    Minor aesthetic changes and code formatting improvements

--- utils/profile_utils.py
+++ utils/profile_utils.py
@@ -5,7 +5,6 @@ with open("profile.json", "r") as f:
 
 
 def dump_profile_json(profile_json):
-    # your code here
     with open("profile.json", "w") as f:
         json.dump(profile_json, f, indent=1)
 
@@ -15,25 +14,25 @@ def create_profile_embed(user: discord.Member) -> tuple:
     profile_embed = discord.Embed(
         title=user.global_name,
         description=f"User: {user.mention}",
-        color=discord.Color.pink()
+        color=discord.Color.pink(),
     ).set_thumbnail(url=user.display_avatar.url)
     with open("gatekeeper.json", "r") as f:
         gate = json.load(f)
-    
-    with open("verification.json", 'r') as f:
+
+    with open("verification.json", "r") as f:
         verification = json.load(f)
 
     for age in gate["age"]:
         if user.guild.get_role(age["role"]) in user.roles:
             user_age = age["label"]
             break
-    if user.guild.get_role(verification['male_role']) in user.roles:
+    if user.guild.get_role(verification["male_role"]) in user.roles:
         user_gender = "Male"
-    if user.guild.get_role(verification['female_role']) in user.roles:
+    if user.guild.get_role(verification["female_role"]) in user.roles:
         user_gender = "Female"
-    if user.guild.get_role(verification['trans_m_role']) in user.roles:
+    if user.guild.get_role(verification["trans_m_role"]) in user.roles:
         user_gender = "Trans M"
-    if user.guild.get_role(verification['trans_f_role']) in user.roles:
+    if user.guild.get_role(verification["trans_f_role"]) in user.roles:
         user_gender = "Trans F"
     user_orientation = user_dm_status = "None"
     for orientation in profile_json["orientation_roles"]:
@@ -44,27 +43,31 @@ def create_profile_embed(user: discord.Member) -> tuple:
         if user.guild.get_role(status["role"]) in user.roles:
             user_dm_status = status["label"]
             break
-    
-    age_verified = user.guild.get_role(verification['18_verified_role'])
-    female_verified = user.guild.get_role(verification['female_gender_verified_role'])
-    male_verified = user.guild.get_role(verification['male_gender_verified_role'])
-    m_trans_verified = user.guild.get_role(verification['f_to_m_verified_role'])
-    f_trans_verified = user.guild.get_role(verification['m_to_f_verified_role'])
+
+    age_verified = user.guild.get_role(verification["18_verified_role"])
+    female_verified = user.guild.get_role(verification["female_gender_verified_role"])
+    male_verified = user.guild.get_role(verification["male_gender_verified_role"])
+    m_trans_verified = user.guild.get_role(verification["f_to_m_verified_role"])
+    f_trans_verified = user.guild.get_role(verification["m_to_f_verified_role"])
     a_verified, g_verified = (False, False)
 
     if age_verified in user.roles:
         a_verified = True
-    
-    if female_verified in user.roles or male_verified in user.roles or m_trans_verified in user.roles or f_trans_verified in user.roles:
+
+    if (
+        female_verified in user.roles
+        or male_verified in user.roles
+        or m_trans_verified in user.roles
+        or f_trans_verified in user.roles
+    ):
         g_verified = True
-    
+
     if a_verified and g_verified:
         user_verification = "Selfie and Age Verified"
     elif g_verified:
         user_verification = "Selfie Verified"
     else:
         user_verification = "Unverified"
-    
 
     profile_embed.add_field(
         name="Name", value=user_profile["name"], inline=True
@@ -73,27 +76,33 @@ def create_profile_embed(user: discord.Member) -> tuple:
     ).add_field(
         name="Orientation", value=user_orientation, inline=True
     ).add_field(
-        name="Location", value=user_profile['location'], inline=True,
+        name="Location",
+        value=user_profile["location"],
+        inline=True,
     ).add_field(
-        name="Dating Status", value = user_profile['status'], inline=True,
+        name="Dating Status",
+        value=user_profile["status"],
+        inline=True,
     ).add_field(
-        name = "Height", value = user_profile['height'], inline=True
+        name="Height", value=user_profile["height"], inline=True
     ).add_field(
-        name='DM Status', value=user_dm_status, inline=True
+        name="DM Status", value=user_dm_status, inline=True
     ).add_field(
-        name='Verification Status', value=user_verification, inline=True
+        name="Verification Status", value=user_verification, inline=True
     ).add_field(
-        name='Looking For', value=user_profile['seeking'], inline=False
+        name="Looking For", value=user_profile["seeking"], inline=False
     ).add_field(
-        name='Hobbies', value=user_profile['hobbies'], inline=False
+        name="Hobbies", value=user_profile["hobbies"], inline=False
     ).add_field(
-        name='About Me', value=user_profile['bio'], inline=False
+        name="About Me", value=user_profile["bio"], inline=False
     )
-    if "selfie" in profile_json['profiles'][str(user.id)]:
-        profile_embed.set_image(url='attachment://img.png')
-        
+    if "selfie" in profile_json["profiles"][str(user.id)]:
+        profile_embed.set_image(url="attachment://img.png")
 
     return profile_embed, str(user_gender)
 
+
 def get_selfie(user: discord.Member):
-    return discord.File(profile_json['profiles'][str(user.id)]['selfie'], filename='img.png')
+    return discord.File(
+        profile_json["profiles"][str(user.id)]["selfie"], filename="img.png"
+    )
